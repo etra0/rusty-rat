addaafdd7fd0790b1cd2dcd5473de297fc16cefb
Sebastián Aedo <sebastian.aedo29@gmail.com>
2021-10-10 13:52:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rusty Rat
+Trackea el precio de los productos en páginas chilenas
+a request.
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use rusty_rat::parsers::Product;
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
